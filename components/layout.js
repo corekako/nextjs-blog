91aa7aa8d846a28029ad3351ca9b512659c0c7d8
corekako <ryo.ando1014@gmail.com>
2021-01-11 18:55:24 +0900

    Render Markdown

--- components/layout.js
+++ components/layout.js
@@ -7,7 +7,6 @@ const name = 'corekako'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
-	console.log(home)
 	return (
 		<div className={styles.container}>
 			<Head>
